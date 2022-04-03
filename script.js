881c9d9388f3355067e6505f62e092052596d6db
cmakeeva <102214309+cmakeeva@users.noreply.github.com>
2022-04-03 19:44:15 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+// #region Список экскурсий
+var modal = document.getElementById("myModal");
+
+function OpenModal() {
+    modal.style.display = "block";
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+// #endregion
+
+
+// #region Обратная связь
+var feedbackModal = document.getElementById("FeedbackModal");
+var span2 = document.getElementsByName("fbClose")[0];
+
+function OpenFeedbackModal() {
+    feedbackModal.style.display = "block";
+}
+
+span2.onclick = function() {
+    feedbackModal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == feedbackModal) {
+        feedbackModal.style.display = "none";
+    }
+}
+//#endregion 
+
+// #region окно проверка данных
+var nameInput = document.getElementsByName('inName')[0];
+var mailInput = document.getElementsByName('iMail')[0];
+var feedbackInput = document.getElementsByName('feedbackinput')[0];
+var statusMsg = document.getElementsByClassName('statusMessage')[0];
+
+var statusModal = document.getElementById("statusModal");
+var okButton = document.getElementsByClassName("btnstatus")[0];
+
+okButton.onclick = function(){
+    statusModal.style.display = "none";
+}
+
+function validateEmail(email) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
+
+document.querySelector('form.feedbackForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    
+    statusModal.style.display = "block";
+    if (validateEmail(mailInput.value)){
+        statusMsg.innerHTML = "Данные успешно обработаны";
+        console.log(nameInput.value);
+        console.log(mailInput.value);
+        console.log(feedbackInput.value);
+    }
+    else {
+        statusMsg.innerHTML = "Почта введена некорректно!";
+    }
+});
+// #endregion
+
